987dbfa40a970921b0a858f2456c41148eadcade
THEJAS-BK <thejasbk998@gmail.com>
2024-12-06 20:38:06 +0530

    new commit

---
+++ new.py
@@ -0,0 +1,22 @@
+file=open("nee.txt",mode="w+")
+
+dict={"thejas":"c41",
+      "sushanth":"c37",
+      "sushanths":"c36"}
+for line in file:
+    new=line.split()
+    for word in new:
+        dict[word]=dict.get(word,0)+1
+print(dict)
+listz=[]
+for key,val in dict.items():
+    listz.append((key,val))
+
+for i in listz:
+    file.write(str(i[0])+" ---->"+str(i[1])+"\n")
+
+file.close()
+
+for lines in open("nee.txt"):
+    nline=lines.split()
+    print(nline[0],"---->",nline[1])
